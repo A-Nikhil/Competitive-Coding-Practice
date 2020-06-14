4dfc1987de0ed7d18d32b7771c3ad447a867fbb2
Nikhil Anand <nikhilanand1006@gmail.com>
2020-06-15 03:57:44 +0530

    Working on Graphs

--- src/graphs/GraphMatrix.java
+++ src/graphs/GraphMatrix.java
@@ -4,7 +4,7 @@ Utility class to return an adjacency matrix from given connections
 
 package graphs;
 
-public class GraphMatrix {
+class GraphMatrix {
 	private final int[][] graph;
 	private final boolean isDirected;
 
@@ -13,7 +13,11 @@ public class GraphMatrix {
 		this.isDirected = isDirected;
 	}
 
-	void addEdge(int source, int destination) {
+	void addEdge(int source, int destination, boolean zeroIndexing) {
+		if (!zeroIndexing) {
+			source--;
+			destination--;
+		}
 		this.graph[source][destination] = 1;
 		if (!isDirected) {
 			this.graph[destination][source] = 1;
@@ -22,5 +26,15 @@ public class GraphMatrix {
 
 	int[][] getGraph() {
 		return this.graph;
+	}
+
+	void printGraphMatrix(int[][] graph) {
+		for (int i = 0; i < graph[0].length; i++) {
+			System.out.print(i + " => ");
+			for (int j = 0; j < graph[0].length; j++) {
+				System.out.print(graph[i][j] + ", ");
+			}
+			System.out.println();
+		}
 	}
 }
