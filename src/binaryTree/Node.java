e6fb457cafb0272101f0da7fadf35f2c6ecbe891
Nikhil Anand <nikhilanand1006@gmail.com>
2020-06-19 03:42:38 +0530

    Moved To Trees

---
+++ src/binaryTree/Node.java
@@ -0,0 +1,10 @@
+package binaryTree;
+
+public class Node {
+	public Node left, right;
+	public int data;
+	public Node(int item) {
+		data = item;
+		left = right = null;
+	}
+}
---
+++ src/binaryTree/traversals/TreeTraversalBasics.java
@@ -0,0 +1,50 @@
+package binaryTree.traversals;
+
+import binaryTree.Node;
+
+public class TreeTraversalBasics {
+	private static void preOrder(Node root) {
+		if (root == null) {
+			return;
+		}
+		System.out.println(root.data);
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+
+	private static void inOrder(Node root) {
+		if (root == null) {
+			return;
+		}
+		inOrder(root.left);
+		System.out.println(root.data);
+		inOrder(root.right);
+	}
+
+	private static void postOrder(Node root) {
+		if (root == null) {
+			return;
+		}
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.println(root.data);
+	}
+
+	public static void main(String[] args) {
+		Node root;
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+
+		System.out.println("Preorder traversal of binary tree is ");
+		preOrder(root);
+
+		System.out.println("\nInorder traversal of binary tree is ");
+		inOrder(root);
+
+		System.out.println("\nPostorder traversal of binary tree is ");
+		postOrder(root);
+	}
+}
