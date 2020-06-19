9763e57256bb65297d7eae33e76b0d2f8f638a46
Nikhil Anand <nikhilanand1006@gmail.com>
2020-06-20 02:16:56 +0530

    Binary Trees

---
+++ src/binaryTree/checkingAndPrinting/ChildSumProperty.java
@@ -0,0 +1,43 @@
+/*
+Check for Children Sum Property in a Binary Tree
+
+Given a binary tree, write a function that returns true if the tree satisfies below property.
+For every node, data value must be equal to sum of data values in left and right children.
+Consider data value as 0 for NULL children. Below tree is an example
+
+https://www.geeksforgeeks.org/check-for-children-sum-property-in-a-binary-tree/
+ */
+
+package binaryTree.checkingAndPrinting;
+
+import binaryTree.Node;
+
+public class ChildSumProperty {
+	private static boolean childSum(Node root) {
+		int sum = 0;
+		if (root == null ||
+				(root.left == null && root.right == null)) {
+			return true;
+		}
+		if (root.left != null) {
+			sum += root.left.data;
+		}
+		if (root.right != null) {
+			sum += root.right.data;
+		}
+		return (sum == root.data) &&
+				childSum(root.left) &&
+				childSum(root.right);
+	}
+
+	public static void main(String[] args) {
+		Node root;
+		root = new Node(10);
+		root.left = new Node(8);
+		root.right = new Node(2);
+		root.left.left = new Node(3);
+		root.left.right = new Node(5);
+		root.right.right = new Node(2);
+		System.out.println(childSum(root));
+	}
+}
