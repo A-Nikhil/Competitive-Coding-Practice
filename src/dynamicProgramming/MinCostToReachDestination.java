6d6696093be28d7888eb05612d291ade39378854
Nikhil Anand <nikhilanand1006@gmail.com>
2020-06-27 19:06:20 +0530

    DP

---
+++ src/dynamicProgramming/MinCostToReachDestination.java
@@ -0,0 +1,30 @@
+package dynamicProgramming;
+
+import java.util.Arrays;
+
+public class MinCostToReachDestination {
+	private static int INF = 99999;
+
+	public static void main(String[] args) {
+		int[][] cost = {
+				{0, 15, 80, 90},
+				{INF, 0, 40, 50},
+				{INF, INF, 0, 70},
+				{INF, INF, INF, 0}
+		};
+		System.out.println(getMinCost(cost, cost.length));
+	}
+
+	private static int getMinCost(int[][] cost, int n) {
+		int[] dist = new int[n];
+		Arrays.fill(dist, INF);
+		dist[0] = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (dist[j] > dist[i] + cost[i][j])
+					dist[j] = dist[i] + cost[i][j];
+			}
+		}
+		return dist[n - 1];
+	}
+}
