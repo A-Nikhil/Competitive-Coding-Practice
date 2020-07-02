35945b2bfdb667ffd813ed66d15a0b835d8cd519
Nikhil Anand <nikhilanand1006@gmail.com>
2020-07-02 20:27:18 +0530

    BackTracking

--- src/Queues/TaskScheduler.java
+++ src/Queues/TaskScheduler.java
@@ -1,12 +1,11 @@
 package Queues;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 public class TaskScheduler {
 	public static void main(String[] args) {
-//		char[] tasks = {'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-		char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+		char[] tasks = {'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+//		char[] tasks = {'A', 'A', 'B', 'B', 'B', 'A'};
 		int n = 2;
 		System.out.println(getTime(tasks, n));
 	}
@@ -15,24 +14,28 @@ public class TaskScheduler {
 		if (n == 0) {
 			return tasks.length;
 		}
-		Queue<Character> queue = new LinkedList<>();
+		int[] count = new int[26];
 		for (char c : tasks) {
-			queue.add(c);
+			count[c - 'A']++;
 		}
-		int count = 0, cool = 0;
-		char curr;
-		while (!queue.isEmpty()) {
-			curr = queue.poll();
-			count++;
-			while (cool != n && !queue.isEmpty()) {
-				cool++;
-				if (curr == queue.peek()) {
-					queue.add(queue.poll());
-				} else {
+		int intervals = 0, i, index;
+		Arrays.sort(count);
+		while (count[25] > 0) {
+			for (i = 0; i <= n; i++) {
+				intervals++;
+				if (i > 25) {
+					continue;
+				}
+				index = 25 - i;
+				if (count[index] > 0) {
+					count[index]--;
+				}
+				if (count[index] == 0 && count[25] == 0) {
 					break;
 				}
 			}
+			Arrays.sort(count);
 		}
-		return count;
+		return intervals;
 	}
 }
