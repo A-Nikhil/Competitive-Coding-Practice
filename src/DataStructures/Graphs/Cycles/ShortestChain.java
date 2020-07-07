b4f76dd1f9a42174b9ee8b398d717aa853746ba3
Nikhil Anand <nikhilanand1006@gmail.com>
2020-07-08 04:08:36 +0530

    Shortest Chain Updated

--- src/DataStructures/Graphs/Cycles/ShortestChain.java
+++ src/DataStructures/Graphs/Cycles/ShortestChain.java
@@ -7,34 +7,43 @@ import java.util.Queue;
 
 public class ShortestChain {
 	public static void main(String[] args) {
-		String[] words = {"POON", "PLEE", "SAME", "POIE", "PLEA", "PLIE", "POIN"};
+//		String[] words = {"POON", "PLEE", "SAME", "POIE", "PLEA", "PLIE", "POIN"};
+//		ArrayList<String> dictionary = new ArrayList<>(Arrays.asList(words));
+//		String start = "TOON";
+//		String find = "POIN";
+		String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
 		ArrayList<String> dictionary = new ArrayList<>(Arrays.asList(words));
-		String start = "TOON";
-		String find = "POIN";
-		new ShortestChain().findChainLength(dictionary, start, find);
+		String start = "hit";
+		String find = "cog";
+//		String[] words = {"a", "b", "c"};
+//		ArrayList<String> dictionary = new ArrayList<>(Arrays.asList(words));
+//		String start = "a";
+//		String find = "c";
+		System.out.println(new ShortestChain().findChainLength(dictionary, start, find));
 	}
 
-	private void findChainLength(ArrayList<String> dictionary, String start, String target) {
-		Queue<String> queue = new LinkedList<>();
-		queue.add(start);
-		int length = 1;
+	private int findChainLength(ArrayList<String> dictionary, String start, String target) {
+		Queue<Entry> queue = new LinkedList<>();
+		boolean[] visited = new boolean[dictionary.size()];
+		if (dictionary.contains(start)) {
+			visited[dictionary.indexOf(start)] = true;
+		}
+		queue.add(new Entry(start, 1));
+		Entry word;
 		while (!queue.isEmpty()) {
-			start = queue.poll();
-			for (String s : dictionary) {
-				if (isAdjacent(start, s)) {
-					queue.add(s);
-					dictionary.remove(s);
-					break;
+			word = queue.poll();
+			start = word.s;
+			if (start.equals(target)) {
+				return word.length;
+			}
+			for (int i = 0; i < dictionary.size(); i++) {
+				if (isAdjacent(start, dictionary.get(i)) && !visited[i]) {
+					queue.add(new Entry(dictionary.get(i), word.length + 1));
+					visited[i] = true;
 				}
 			}
-			start = queue.peek();
-			assert start != null;
-			if (start.equalsIgnoreCase(target)) {
-				break;
-			}
-			length++;
 		}
-		System.out.println("Length = " + (length + 1));
+		return -1;
 	}
 
 	private boolean isAdjacent(String a, String b) {
@@ -48,5 +57,15 @@ public class ShortestChain {
 			}
 		}
 		return count == 1;
+	}
+
+	static class Entry {
+		String s;
+		int length;
+
+		Entry(String s, int length) {
+			this.s = s;
+			this.length = length;
+		}
 	}
 }
