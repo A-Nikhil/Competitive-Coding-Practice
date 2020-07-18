f3fd06bf356ed582d0de33a68fda889314b0640f
Nikhil Anand <nikhilanand1006@gmail.com>
2020-07-18 22:27:21 +0530

    prep

---
+++ src/Commons/LeetCodeListMaker.java
@@ -0,0 +1,27 @@
+// Constructing a LinkedList from a LeetCode input
+
+package Commons;
+
+public class LeetCodeListMaker {
+	ListNode<Integer> head;
+
+	public LeetCodeListMaker() {
+		this.head = null;
+	}
+
+	public ListNode<Integer> makeList(String x) {
+		ListNode<Integer> temp = null, newNode;
+		String[] elems = x.split("->");
+		for (int i = 0; i < elems.length - 1; i++) {
+			newNode = new ListNode<>(Integer.parseInt(elems[i]));
+			if (this.head == null) {
+				head = newNode;
+			} else {
+				assert temp != null;
+				temp.next = newNode;
+			}
+			temp = newNode;
+		}
+		return this.head;
+	}
+}
--- src/DataStructures/LinkedList/SinglyLinkedLists/ReverseList2.java
+++ src/DataStructures/LinkedList/SinglyLinkedLists/ReverseList2.java
@@ -1,4 +1,63 @@
 package DataStructures.LinkedList.SinglyLinkedLists;
 
+import Commons.LeetCodeListMaker;
+import Commons.ListNode;
+
 public class ReverseList2 {
+	public static void main(String[] args) {
+		ListNode<Integer> head;
+		ListNode<Integer> list = new ListNode<>();
+
+		// Making List from LeetCode Input
+		String x = "1->2->3->4->5->NULL";
+//		String x = "3->5->NULL";
+		LeetCodeListMaker maker = new LeetCodeListMaker();
+		head = maker.makeList(x);
+		list.printList(head);
+		System.out.println();
+
+		int m = 2, n = 4;
+//		int m = 1, n = 2;
+		list.printList(reverse(head, m, n));
+	}
+
+	private static ListNode<Integer> reverse(ListNode<Integer> head, int m, int n) {
+		if (head == null) {
+			return null;
+		}
+
+		if (m == n) {
+			return head;
+		}
+		int index = 1;
+		ListNode<Integer> temp = head;
+		ListNode<Integer> auxillary = null, auxEnd = null;
+		ListNode<Integer> start = null;
+		ListNode<Integer> newNode;
+		while (temp != null) {
+			if (index < m) {
+				start = temp;
+			} else if (index >= m && index <= n) {
+				newNode = new ListNode<>(temp.data);
+				if (auxillary == null) {
+					auxEnd = newNode;
+				}
+				newNode.next = auxillary;
+				auxillary = newNode;
+			} else {
+
+				break;
+			}
+			temp = temp.next;
+			index++;
+		}
+		if (start == null) {
+			head = auxillary;
+		} else {
+			start.next = auxillary;
+		}
+		assert auxEnd != null;
+		auxEnd.next = temp;
+		return head;
+	}
 }
