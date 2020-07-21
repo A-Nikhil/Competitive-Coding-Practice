a9e07c93cf89be7e372ad5b3bcc9753d2bc76747
Nikhil Anand <nikhilanand1006@gmail.com>
2020-07-21 22:32:03 +0530

    Leetcode prep

---
+++ src/DataStructures/BinaryTree/CheckingAndPrinting/GoodNodes.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+package DataStructures.BinaryTree.CheckingAndPrinting;
+
+import Commons.Node;
+
+public class GoodNodes {
+	private static int count;
+
+	private static int goodNodes(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		count = 0;
+		helper(root, Integer.MIN_VALUE);
+		return count;
+	}
+
+	private static void helper(Node root, int max) {
+		if (root == null) {
+			return;
+		}
+
+		if (root.data >= max) {
+			count++;
+		}
+
+		helper(root.left, Math.max(max, root.data));
+		helper(root.right, Math.max(max, root.data));
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(3);
+		
+	}
+}
---
+++ src/DataStructures/BinaryTree/Misc/HouseRobber3.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/house-robber-iii/
+
+package DataStructures.BinaryTree.Misc;
+
+import Commons.Node;
+
+public class HouseRobber3 {
+	private static int rob(Node root) {
+		if (root == null) {
+			return 0;
+		}
+
+		int[] result = helper(root);
+		return Math.max(result[0], result[1]);
+	}
+
+	private static int[] helper(Node root) {
+		if (root == null) {
+			return new int[]{0, 0};
+		}
+
+		int[] result = new int[2];
+		int[] left = helper(root.left);
+		int[] right = helper(root.right);
+
+		// result[0] is when root is selected, result[1] is when not.
+		result[0] = root.data + left[1] + right[1];
+		result[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		/*
+		     3
+			/ \
+		   4   5
+		  / \   \
+		 1   3   1
+
+		*/
+		Node root = new Node(3);
+		root.left = new Node(4);
+		root.right = new Node(5);
+		root.left.left = new Node(1);
+		root.left.right = new Node(3);
+		root.right.right = new Node(1);
+
+		System.out.println(rob(root));
+	}
+}
