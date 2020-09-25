a27eb08f4b4ce3145954c1bee7430323291903a9
Nikhil Anand <nikhilanand1006@gmail.com>
2020-09-25 20:51:41 +0530

    Codes Added

---
+++ src/LeetCode/LargestNumber.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+import DataStructures.Arrays.Misc.LargestTime;
+
+import java.util.Arrays;
+
+public class LargestNumber {
+	public static void main(String[] args) {
+		int[] nums = {10, 2};
+		System.out.println(new LargestTime().largestTimeFromDigits(nums));
+	}
+
+	public String largestNumber(int[] nums) {
+		String[] combos = new String[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			combos[i] = String.valueOf(nums[i]);
+		}
+		Arrays.sort(combos, (o1, o2) -> {
+			String s1 = o1 + o2;
+			String s2 = o2 + o1;
+			return s2.compareTo(s1);
+		});
+		if ("0".equals(combos[0])) {
+			return combos[0];
+		}
+		StringBuilder sb = new StringBuilder();
+		for (String str : combos) {
+			sb.append(str);
+		}
+		return sb.toString();
+	}
+}
